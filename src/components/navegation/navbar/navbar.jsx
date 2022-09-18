c775832b16187b9d23ad2f87a5215ead3d773611
Carlos Cruz <carturo99@gmail.com>
2022-09-17 22:57:41 -0500

    create navbar component

---
+++ src/components/navegation/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Proyectos</li>
+                <li>Habilidades</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
