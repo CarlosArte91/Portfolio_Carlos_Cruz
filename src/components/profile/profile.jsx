19a8d69c2699390eb2a1dbdb6ca94b3672456754
Carlos Cruz <carturo99@gmail.com>
2022-09-18 20:58:59 -0500

    profile styles

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,3 +1,5 @@
+import aboutMe from '../../assets/data/aboutMe.js';
+
 import profileImage from '../../assets/images/profile/profile.JPG';
 import Socials from '../contact/socials/socials';
 import styles from './profile.module.css';
@@ -7,7 +9,7 @@ function Profile() {
         <div className={styles.container}>
             <h1 className={styles.name}>Carlos Cruz</h1>
             <h2 className={styles.profession}>Full Stack Web Developer</h2>            
-            <p></p>
+            <p className={styles.aboutme}>{aboutMe}</p>
             <img className={styles.profileImage} src={profileImage} alt="profile" />
             <div className={styles.socials}><Socials/></div>            
         </div>
